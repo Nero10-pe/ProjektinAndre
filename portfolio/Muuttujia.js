e6c783de294d387f6910d1815ca00964bac096f6
Cerdan Rios <cerdan.rios@kukkula.local>
2025-05-02 14:08:59 +0300

    New

--- portfolio/Muuttujia.js
+++ portfolio/Muuttujia.js
@@ -3,6 +3,7 @@ let b = 5;
 let c = 7;
 let d = 15;
 
+
 function summa() {
     alert(a + b);
 }
@@ -17,4 +18,48 @@ function kerto() {
 
 function jako() {
     alert(d / a);
+}
+
+function laskin_summa() {
+    const eka = parseFloat(document.getElementById('numero1').value);
+    const toka = parseFloat(document.getElementById('numero2').value);
+    alert(summa(eka, toka));
+}
+
+function laskin_vahennys() {
+    const eka = parseFloat(document.getElementById('numero1').value);
+    const toka = parseFloat(document.getElementById('numero2').value);
+    alert(vahennys(eka, toka));
+}
+
+function laskin_kerto() {
+    const eka = parseFloat(document.getElementById('numero1').value);
+    const toka = parseFloat(document.getElementById('numero2').value);
+    alert(kerto(eka, toka));
+}
+
+function laskin_jako() {
+    const eka = parseFloat(document.getElementById('numero1').value);
+    const toka = parseFloat(document.getElementById('numero2').value);
+    if (toka !== 0) {
+        alert(jako(eka, toka));
+    } else {
+        alert("Nolla ei voi olla jakajana.");
+    }
+}
+
+function summaCalc(a, b) {
+    return a + b;
+}
+
+function vahennysCalc(a, b) {
+    return a - b;
+}
+
+function kertoCalc(a, b) {
+    return a * b;
+}
+
+function jakoCalc(a, b) {
+    return a / b;
 }
