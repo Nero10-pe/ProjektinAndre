5ddc9f98968046b4b79df2fe9c91b5e33c345db3
Cerdan Rios <cerdan.rios@kukkula.local>
2025-05-09 09:47:46 +0300

    New

--- portfolio/Muuttujia.js
+++ portfolio/Muuttujia.js
@@ -4,62 +4,47 @@ let c = 7;
 let d = 15;
 
 
-function summa() {
-    alert(a + b);
+function summa(a,b) {
+    alert("Summa: " + (a + b));
 }
 
-function vahennys() {
-    alert(b - c);
+function vahennys(b,c) {
+    alert("Vahennys: " + (b - c));
 }
 
-function kerto() {
-    alert(c * d);
+function kerto(c,d) {
+    alert("Kerto:" + (c * d));
 }
 
-function jako() {
-    alert(d / a);
+function jako(d,a) {
+    alert("Jako: " + (d / a));
 }
 
 function laskin_summa() {
     const eka = parseFloat(document.getElementById('numero1').value);
     const toka = parseFloat(document.getElementById('numero2').value);
-    alert(summa(eka, toka));
+    summa(eka, toka)
 }
 
 function laskin_vahennys() {
     const eka = parseFloat(document.getElementById('numero1').value);
     const toka = parseFloat(document.getElementById('numero2').value);
-    alert(vahennys(eka, toka));
+    vahennys(eka, toka)
+
 }
 
 function laskin_kerto() {
     const eka = parseFloat(document.getElementById('numero1').value);
     const toka = parseFloat(document.getElementById('numero2').value);
-    alert(kerto(eka, toka));
+    kerto(eka, toka)
 }
 
 function laskin_jako() {
     const eka = parseFloat(document.getElementById('numero1').value);
     const toka = parseFloat(document.getElementById('numero2').value);
     if (toka !== 0) {
-        alert(jako(eka, toka));
+        jako(eka, toka);
     } else {
         alert("Nolla ei voi olla jakajana.");
     }
-}
-
-function summaCalc(a, b) {
-    return a + b;
-}
-
-function vahennysCalc(a, b) {
-    return a - b;
-}
-
-function kertoCalc(a, b) {
-    return a * b;
-}
-
-function jakoCalc(a, b) {
-    return a / b;
 }
